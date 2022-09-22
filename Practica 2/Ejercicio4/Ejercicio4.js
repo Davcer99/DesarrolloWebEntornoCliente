97ccbbbe5375aa81d5b77979703aab96fca25876
Davcer99 <davidbalaguercervera@gmail.com>
2022-09-22 13:31:16 +0200

    Correccion ejercicio

--- Practica 2/Ejercicio4/Ejercicio4.js
+++ Practica 2/Ejercicio4/Ejercicio4.js
@@ -1,6 +1,8 @@
 'use strict'
+let myWindow;
 if (navigator.userAgent != 'msie'){
-    window.resizeTo(500,500);
+    myWindow = window.open("", "", "width=200, height=100");
+    myWindow = resizeTo(300,300);
 }else{
     alert("Operación no permitida");
 };
