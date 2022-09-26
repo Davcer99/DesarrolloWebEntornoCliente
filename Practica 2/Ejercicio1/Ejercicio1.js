69f4b467a72e97ccd24a997f7fa8993d7b118466
Davcer99 <davidbalaguercervera@gmail.com>
2022-09-26 11:23:29 +0200

    Corrección ejercicio1

--- Practica 2/Ejercicio1/Ejercicio1.js
+++ Practica 2/Ejercicio1/Ejercicio1.js
@@ -2,11 +2,12 @@
 var precio = parseFloat(prompt("Introduce el precio del articulo"));
 var iva = precio * 0.21
 var precioTotal = calcularIva(precio,21);
+console.log(precioTotal);
 alert("El iva del producto es: " + iva.toFixed(2));
 alert("El precio total de producto es: " + precioTotal.toFixed(2));
 
 function calcularIva(precio,iva) {
-    var ivaMult = iva/100;
-    var resultado = precio * iva;
+    var ivaMult = precio * (iva/100);
+    var resultado = precio + ivaMult;
     return resultado;
 }
