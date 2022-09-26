5733d59ee8a594c983db934081390565c0002f7f
Davcer99 <davidbalaguercervera@gmail.com>
2022-09-26 12:07:45 +0200

    Ejercicios hechos en clase

---
+++ Practica 2/Anexo/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,4 @@
+'use strict'
+function avisar() {
+    alert("Estas sobre un hipervinculo");
+}
---
+++ Practica 2/Anexo/Ejercicio2/Ejercicio2.js
@@ -0,0 +1,8 @@
+'use strict'
+function mostrarDni() {
+    var dni = document.getElementById('dni').value;    
+    if(dni == ""){
+        alert("Introduce un dni")
+    }
+}
+
