d1790d00ac4b2ccd94f5a5850bb34cb4c8022664
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-03 12:27:28 +0200

    Ejercicio4

--- Practica 3/Ejercicio4/Ejercicio4.js
+++ Practica 3/Ejercicio4/Ejercicio4.js
@@ -1,14 +1,20 @@
 'use strict'
+window.onload = function(){
+    var botonEnlaces = document.getElementById('enlaces');
+    var botonimagenes = document.getElementById('imagenes')
+    botonEnlaces.addEventListener("click",enlaces);
+    botonimagenes.addEventListener("click",imagenes);
+    function enlaces() {
+        var contenido = document.getElementsByTagName('a');
+        alert(contenido.length);
+        alert("Hay " + (contenido.length) + " links en la página")
+    }
+    function imagenes() {
+        var contenido = document.getElementsByTagName('img');
+        alert("Hay " + (contenido.length ) + " imagenes en la página")
+    }
+}
 
-function enlaces() {
-    var contenido = document.getElementsByTagName('a');
-    alert(contenido.length);
-    alert("Hay " + (contenido.length) + " links en la página")
-}
-function imagenes() {
-    var contenido = document.getElementsByTagName('img');
-    alert("Hay " + (contenido.length ) + " imagenes en la página")
-}
 
     
     
