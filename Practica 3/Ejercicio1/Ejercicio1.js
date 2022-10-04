615e150b61302d49ae1361aa2f7a8183b3ba29d3
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-04 12:05:33 +0200

    Ejercicio 1

---
+++ Practica 3/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,7 @@
+'use strict'
+window.onload = function(){
+    var agregar = document.getElementById('agregar');
+    var insertar = document.getElementById('insertar');
+    var reemplazar = document.getElementById('reemplazar');
+    
+}
