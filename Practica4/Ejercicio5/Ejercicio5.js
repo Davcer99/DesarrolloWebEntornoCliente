aa77573a1356b7b1bbe4e5b914b0bba6b1dfbb17
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-24 11:56:08 +0200

    Ejercicio 5

---
+++ Practica4/Ejercicio5/Ejercicio5.js
@@ -0,0 +1,18 @@
+window.onload =  function(){
+    var input = document.getElementById('documento');
+    input.addEventListener("keypress", formatear_texto)
+}
+function formatear_texto(event) {
+    
+    if (event.charCode >= '48' && event.charCode <= '57'){
+       
+    }else{
+        event.preventDefault();
+    }
+}
+
+
+
+
+
+
