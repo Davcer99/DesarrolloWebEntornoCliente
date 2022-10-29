ea32ab56280ecd5c08eb925a58136b4e6f8c1e84
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-29 15:38:01 +0200

    Modificación en el menu

--- PaginaWeb/javascript/menu.js
+++ PaginaWeb/javascript/menu.js
@@ -5,5 +5,12 @@ window.onload = function(){
 
 function desplegar(event) {
     let menu = document.getElementById('barra_de_navegacion');
+    let menuToogleIcon = document.querySelector('.menu-toogle i')
     menu.classList.toggle('show');
+
+    if(menu.classList.contains('show')){
+        menuToogleIcon.setAttribute('class','fa fa-times');
+    }else{
+        menuToogleIcon.setAttribute('class','fa fa-bars')
+    }
 }
