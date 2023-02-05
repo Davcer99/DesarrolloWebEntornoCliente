05fba33de9f9264603657e8a848e9c14b0744d2c
Davcer99 <davidbalaguercervera@gmail.com>
2023-02-05 18:45:30 +0100

    Ejercicio4 hecho

--- Tema 3/Practica4/Ejercicio4/Ejercicio4.js
+++ Tema 3/Practica4/Ejercicio4/Ejercicio4.js
@@ -1,1 +1,9 @@
-$(".horizontal-slider").slick();
+$(document).ready(function(){
+    $('.carrousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+  });
